c58db8a1cc95a3a2f3dc8036eb7e696ab1549e73
yum <yusufmandirali@web.de>
2025-07-06 00:48:29 +0200

    Add Buffering for outputters, all by default are fully buffered, to align with flushing policies

--- include/lfy/Outputter.hpp
+++ include/lfy/Outputter.hpp
@@ -4,13 +4,28 @@
 
 #pragma once
 
+#include <cstddef>
 #include <filesystem>
 #include <fstream>
+#include <ios>
 #include <iostream>
 #include <mutex>
 #include <print>
+#include <vector>
 
 namespace lfy {
+
+namespace literals {
+
+constexpr std::size_t operator""_KiB(unsigned long long x) { return x << 10; }
+constexpr std::size_t operator""_MiB(unsigned long long x) { return x << 20; }
+constexpr std::size_t operator""_GiB(unsigned long long x) { return x << 30; }
+
+constexpr std::size_t KiB = 1_KiB;
+constexpr std::size_t MiB = 1_MiB;
+constexpr std::size_t GiB = 1_GiB;
+
+} // namespace literals
 
 class Outputter {
 public:
@@ -21,12 +36,16 @@ public:
 
 class ConsoleOutputter : public Outputter {
 public:
-  ConsoleOutputter() = default;
+  ConsoleOutputter(std::size_t bufferSize = 4 * literals::KiB) {
+    m_buffer.reserve(bufferSize);
+    std::cout.rdbuf()->pubsetbuf(m_buffer.data(), m_buffer.capacity());
+  }
   ~ConsoleOutputter() = default;
 
   void output(const std::string &message) override {
     std::lock_guard l{m_mutex};
-    std::println(std::cout, "{}", message);
+    // Don't use std::print, as it *always* causes flush for terminal output
+    std::cout << message << "\n";
   }
 
   void flush() override {
@@ -36,18 +55,25 @@ public:
 
 private:
   std::mutex m_mutex;
+  std::vector<char> m_buffer;
 };
 
 class FileOutputter : public Outputter {
 public:
-  explicit FileOutputter(const std::filesystem::path filePath)
+  explicit FileOutputter(const std::filesystem::path filePath,
+                         std::size_t bufferSize = 16 * literals::KiB)
       : m_filePath{std::move(filePath)} {
     if (std::filesystem::exists(m_filePath) &&
         !std::filesystem::is_regular_file(m_filePath))
       throw std::runtime_error("FileOutputter: File" + m_filePath.string() +
                                " does not exist or is not a regular file.");
-
     m_filestream.open(m_filePath, std::ios::out | std::ios::app);
+    if (!m_filestream.is_open())
+      throw std::runtime_error("FileOutputter: Failed to open file " +
+                               m_filePath.string());
+
+    m_buffer.reserve(bufferSize);
+    m_filestream.rdbuf()->pubsetbuf(m_buffer.data(), m_buffer.capacity());
   }
 
   ~FileOutputter() = default;
@@ -66,6 +92,7 @@ public:
 private:
   std::mutex m_mutex;
   std::filesystem::path m_filePath;
+  std::vector<char> m_buffer;
   std::ofstream m_filestream;
 };
 
