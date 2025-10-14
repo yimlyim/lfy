4933d1ba5efc8fdbea15af10edc577e16bd23a37
yum <yusufmandirali@web.de>
2025-10-15 00:59:13 +0200

    Add some safety check, to prevent some user errors in plain array handling

--- include/lfy/Types.hpp
+++ include/lfy/Types.hpp
@@ -7,10 +7,19 @@
 
 namespace lfy {
 
-enum class LogLevel { Debug = 0, Info = 1, Warn = 2, Error = 3 };
+enum class LogLevel {
+  Debug = 0,
+  Info = 1,
+  Warn = 2,
+  Error = 3,
+  NumberOfLevels = 4
+};
 
 constexpr std::string_view logLevelToString(LogLevel level) {
   constexpr static std::array logLevelMap = {"DEBUG", "INFO", "WARN", "ERROR"};
+  static_assert(logLevelMap.size() ==
+                    static_cast<size_t>(LogLevel::NumberOfLevels),
+                "logLevelMap size must match number of LogLevel entries");
   return logLevelMap[static_cast<size_t>(level)];
 }
 
